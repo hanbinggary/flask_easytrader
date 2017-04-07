4aac7d4263ff624cefc23e8ac72dc8bc508fc6d6
marx99 <maxingzhe99@126.com>
2017-04-07 16:53:26 +0800

    高频数据采集表示基本完成
    暂没有成交功能

---
+++ easyquotation/basequotation.py
@@ -0,0 +1,104 @@
+# coding:utf8
+import asyncio
+import json
+import warnings
+
+import aiohttp
+import easyutils
+import yarl
+
+from . import helpers
+
+
+class BaseQuotation:
+    """行情获取基类"""
+    max_num = 800  # 每次请求的最大股票数
+    stock_api = ''  # 股票 api
+
+    def __init__(self):
+        self._session = None
+        stock_codes = self.load_stock_codes()
+        self.stock_list = self.gen_stock_list(stock_codes)
+
+    def gen_stock_list(self, stock_codes):
+        stock_with_exchange_list = [easyutils.stock.get_stock_type(code) + code[-6:] for code in stock_codes]
+
+        if len(stock_with_exchange_list) < self.max_num:
+            request_list = ','.join(stock_with_exchange_list)
+            return [request_list]
+
+        stock_list = []
+        request_num = len(stock_codes) // self.max_num + 1
+        for range_start in range(request_num):
+            num_start = self.max_num * range_start
+            num_end = self.max_num * (range_start + 1)
+            request_list = ','.join(stock_with_exchange_list[num_start:num_end])
+            stock_list.append(request_list)
+        return stock_list
+
+    @staticmethod
+    def load_stock_codes():
+        with open(helpers.stock_code_path()) as f:
+            return json.load(f)['stock']
+
+    @property
+    def all(self):
+        warnings.warn('use all_market instead', DeprecationWarning)
+        return self.get_stock_data(self.stock_list)
+
+    @property
+    def all_market(self):
+        """return quotation with stock_code prefix key"""
+        return self.get_stock_data(self.stock_list, prefix=True)
+
+    def stocks(self, stock_codes):
+        if type(stock_codes) is not list:
+            stock_codes = [stock_codes]
+
+        stock_list = self.gen_stock_list(stock_codes)
+        return self.get_stock_data(stock_list)
+
+    def fetch_stocks(self, stock_codes):
+        if type(stock_codes) is not list:
+            stock_codes = [stock_codes]
+
+        stock_list = self.gen_stock_list(stock_codes)
+        return self.get_stock_data(stock_list, prefix=True)
+
+    async def get_stocks_by_range(self, params):
+        if self._session is None:
+            #conn = aiohttp.ProxyConnector(proxy="http://10.88.42.18:8080")
+            self._session = aiohttp.ClientSession()
+        headers = {
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.100 Safari/537.36'
+        }
+        url = yarl.URL(self.stock_api + params, encoded=True)
+        try:
+            async with self._session.get(url, timeout=10, headers=headers,proxy='http://10.88.42.18:8080') as r:
+                response_text = await r.text()
+                return response_text
+        except asyncio.TimeoutError:
+            return None
+
+    def get_stock_data(self, stock_list, **kwargs):
+        coroutines = []
+
+        for params in stock_list:
+            coroutine = self.get_stocks_by_range(params)
+            coroutines.append(coroutine)
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+        res = loop.run_until_complete(asyncio.gather(*coroutines))
+
+        return self.format_response_data([x for x in res if x is not None], **kwargs)
+
+    def __del__(self):
+        if self._session is not None:
+            self._session.close()
+
+    def format_response_data(self, rep_data, **kwargs):
+        pass
--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from flask import Flask
 import requests
+import easyquotation
+import json 
 
 app = Flask(__name__)
 
@@ -40,6 +42,30 @@ def post_stock(stock='sz000001'):
     }
     html = requests.get('http://hq.sinajs.cn/?format=text&list=%s' % stock,proxies=proxies)
     return html.text
+
+@app.route('/qq/<stock>')
+@app.route('/hello/qq/')
+@app.route('/qq/')
+def tq_test(stock='000001'):
+    q = easyquotation.use('qq')
+#    print (time.time())
+#    print (q.stocks(['000001','000002','000005', '162411']))
+#    print (time.time())
+    stock_list=['002858','603041','002857','603388','603178','002816','603031','603701','603991','002806','603319','002796','603090','603038','603990','603029','002800','603908','002810','002837','002835','603738','002805','603960','603266','603037','603819','603633','603887','002856','603033','603663','002830','603637','603089','603032','002808']
+    stockinfo,stockinfo_zhangting = q.stocks(stock_list)
+#    print(stockinfo.get('000001'))
+    #json_obj = json.dumps(dict(stock=stockinfo),ensure_ascii=False)
+#    print(json_obj)
+    temp = sorted(stockinfo.items(), key=lambda d:d[1]['流通市值'])
+#    for key ,value in temp:
+#        print(value['PB'])
+    #print(json.dumps(dict(temp)))
+	#最小流通市值取得
+    min_liutong = min(stockinfo.items(), key=lambda d:d[1]['流通市值'])[1]['流通市值']
+	#计算流通市值差
+    for key,value in stockinfo.items():
+        stockinfo[key]['cha'] = round((float(stockinfo[key]['流通市值'])/float(min_liutong) - 1)*100,2)
+    return render_template('post_test.html', entries=stockinfo,stockinfo_zhangting=stockinfo_zhangting,stockinfo_sort=temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
